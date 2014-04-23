2fd50a8f77057e080e145b61c8b2a81a7617a7f4
Alohany <rpaley@rccl.com>
2014-04-22 20:11:35 -0400

    Inverts a matrix and caches the results
    Calculates the inverse of a matrix and caches the results.  Cache can be
    retrieved for future calculations without reprocessing

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow the user to calculate the inverse of a matrix using the R solve()
+## function and then cache the inverse for subsequent retrieval.
 
-## Write a short comment describing this function
+## The functions assume that the matrix is invertible and has not changed over time.  The
+## functions reduce the processing overhead of repeatedly calculating the inverse of the same 
+## function.
+
+## This function constructs a "special" vector containing four functions that:
+## 1 - set the value of the matrix
+## 2 - get the value of the matrix
+## 3 - set the inverse of the matrix
+## 4 - get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    m<-NULL
+    
+    set <- function (y) {
+        
+        x <<- y
+        m <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) m<<-inverse
+    
+    getinverse <- function() m
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+    
 }
 
+## This function checks to see if the inverse of the matrix has already been cached
+## If it has been, the inverse is returned.  If not, the inverse is calculated using
+## the solve function and then stored for future retrieval
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+    
+    l <- makeCacheMatrix(x)
+    
+    m<-l$getinverse()
+    
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    
+    data<-l$get()
+    
+    m<-solve(data,...)
+    
+    l$setinverse(m)
+    
+    m
 }
